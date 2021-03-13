1326be3bb1075a8ea251545f6cf9d30538047f75
DimasBagasSaputro <dimazzbagazz@student.telkomuniversity.ac.id>
2021-03-13 19:02:44 +0700

    Membuat navigasi ke app lain

--- app/src/main/java/com/d3if2089/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/com/d3if2089/hitungbmi/ui/HitungFragment.kt
@@ -1,5 +1,6 @@
 package com.d3if2089.hitungbmi.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.*
@@ -28,6 +29,7 @@ class HitungFragment : Fragment() {
         binding.saranButton.setOnClickListener { view : View ->
             view.findNavController().navigate(HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi))
         }
+        binding.shareButton.setOnClickListener {shareData()}
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -57,7 +59,7 @@ class HitungFragment : Fragment() {
 
         binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
         binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-        binding.saranButton.visibility = View.VISIBLE
+        binding.buttonGroup.visibility = View.VISIBLE
     }
 
     private fun getKategori(bmi : Float, isMale: Boolean): String {
@@ -89,6 +91,26 @@ class HitungFragment : Fragment() {
         binding.bmiTextView.text = null
         binding.kategoriTextView.text = null
         binding.beratEditText.requestFocus()
+    }
+
+    private fun shareData() {
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val gender = if (selectedId == R.id.priaRadioButton)
+            getString(R.string.pria)
+        else
+            getString(R.string.wanita)
+        val message = getString(R.string.bagikan_template,
+        binding.beratEditText.text,
+        binding.tinggiEditText.text,
+        gender,
+        binding.bmiTextView.text,
+        binding.kategoriTextView.text)
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(shareIntent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
