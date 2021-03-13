8222270bd645fe82103447618be267f9531f0da5
DimasBagasSaputro <dimazzbagazz@student.telkomuniversity.ac.id>
2021-03-13 17:50:27 +0700

    initial commit

---
+++ app/src/main/java/com/d3if2089/hitungbmi/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.d3if2089.hitungbmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/d3if2089/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,3 @@
+package com.d3if2089.hitungbmi.data
+
+enum class KategoriBmi { KURUS, IDEAL, GEMUK }
